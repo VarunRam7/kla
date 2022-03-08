a9e3c3411d17c1afbc6cd850a6d99afd6b6999cf
iamvarun <varunram35@gmail.com>
2022-03-08 11:02:35 +0530

    1st Commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import yaml
+import time
+import datetime
+from yaml.loader import SafeLoader
+
+file1A = open("Log.txt", "w")
+
+
+def waitingTime(typeName, taskName, input, secs):
+    ct1 = str(datetime.datetime.now())
+    string = ct1 + ';' + typeName + '.' + taskName + ' Executing TimeFunction(' + input + ',' + secs + ')'
+    file1A.write(string + '\n')
+    time.sleep(int(secs))
+
+
+def seq(typeName, activities):
+    for i in activities.keys():
+        if activities[i]['Type'] == 'Task':
+            if activities[i]['Function'] == "TimeFunction":
+                ct1 = str(datetime.datetime.now())
+                string = ct1 + ';' + typeName + '.' + i + ' Entry'
+                file1A.write(string + '\n')
+                waitingTime(typeName, i, activities[i]['Inputs']['FunctionInput'], activities[i]['Inputs']['ExecutionTime'])
+                ct2 = str(datetime.datetime.now())
+                string1 = ct2 + ';' + typeName + '.' + i + ' Exit'
+                file1A.write(string1 + '\n')
+
+        if activities[i]['Type'] == 'Flow':
+            ct1 = str(datetime.datetime.now())
+            string = ct1 + ';' + typeName + '.' + i + ' Entry'
+            file1A.write(string + '\n')
+            seq(typeName + '.' + i, activities[i]['Activities'])
+            ct1 = str(datetime.datetime.now())
+            string = ct1 + ';' + typeName + '.' + i + ' Exit'
+            file1A.write(string + '\n')
+
+
+with open('Milestone1A.yaml') as f:
+    data1 = yaml.load(f, Loader=SafeLoader)
+f.close()
+
+with open('Milestone1B.yaml') as f:
+    data2 = yaml.load(f, Loader=SafeLoader)
+f.close()
+
+for i in data1.keys():
+    timestamp = str(datetime.datetime.now())
+    st = timestamp + ';' + i + ' Entry'
+    file1A.write(st + '\n')
+    if data1[i]['Type'] == "Flow":
+        if data1[i]['Execution'] == 'Sequential':
+            seq(i, data1[i]['Activities'])
+    timestamp = str(datetime.datetime.now())
+    st = timestamp + ';' + i + ' Exit'
+    file1A.write(st + '\n')
